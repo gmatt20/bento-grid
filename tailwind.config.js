ea9e8d4d83a46ff0585b046f84eae2a098831ff4
Matthew Garcia <gmatthew2765@gmail.com>
2024-11-19 17:40:23 -0800

    Added custom colors and box-shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ module.exports = {
         "custom-columns": "1fr 1fr 1fr 1fr",
         "custom-rows": "1fr 1fr 1fr",
       },
+      colors: {
+        "purple-custom-100": "#dacffc",
+        "purple-custom-500": "#7650dc",
+        "yellow-custom-100": "#f9eee1",
+        "yellow-custom-500": "#ffcb6b",
+      },
     },
   },
   plugins: [],
